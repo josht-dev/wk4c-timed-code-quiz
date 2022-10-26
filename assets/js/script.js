727a0ab7a2909011b3938c75daf6eb3b07c1a805
Joshua Taylor <toasterrage@gmail.com>
2022-10-26 14:39:43 -0600

    Renamed scripts folder to js, Updated HTML, CSS, and JS with planning comments

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+/*
+---------- User Story ----------
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+---------- Acceptance Criteria ----------
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+*/
+
+/* TO DO - Object that stores arrays of question/answer key/value pairs
+ex: obj {
+    question1: {
+        question:
+    }
+}
+    OR?
+array [
+    {
+        question: "question str here",
+        multiChoice1: "possible answer",
+        multiChoice4: "possible answer",
+        correctAnswer: this.multiChoice1
+    },
+    {
+        question: "question str here",
+        multiChoice1: "possible answer",
+        multiChoice4: "possible answer",
+        correctAnswer: this.multiChoice4
+    }
+]
+*/
+
+/* TO DO - Locally store high scores...obj again?
+ex: obj {
+    "initials": 42,
+    "initials": 0,
+}
+*/
+
+// TO DO - Start timer, end timer code block...
+
+// TO DO - Subtract time from wrong answer code block...
+
+// TO DO - Hide quiz title in html
+
+// TO DO - Generate question/answer HTML code block by looping
+
+// TO DO - Wait for user input, then evaluate answer
+
+// TO DO - Subtract from timer for wrong answer
+
+// TO DO - End quiz at end of timer
+
+// TO DO - End quiz when all questions answered
+
+// TO DO - Generate end of quiz HTML
+
+// TO DO - Check for high score
+
+// TO DO - Generate HTML for high score list
